645394540c96634d50d1f6d02bf6c8cfd28d03ba
Robert Bautista <robert.bautista999@gmail.com>
2023-11-06 21:43:17 -0800

    CHAAAANGES

---
+++ PZ/scripts/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p "${STEAMAPPDIR}" || true
+
+if [ -z "$STEAMCMD_UPDATE_ARGS" ]; then
+	bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "$STEAMAPPDIR" +login anonymous +app_update "$STEAMAPPID" +quit
+else
+	steamcmd_update_args=($STEAMCMD_UPDATE_ARGS)
+	bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "$STEAMAPPDIR" +login anonymous +app_update "$STEAMAPPID" "${steamcmd_update_args[@]}" +quit
+fi
